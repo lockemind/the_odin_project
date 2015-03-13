4bdbb265fc29d4dda12012e40bcc421f9c895bb2
lockemind <pedr.amaral@gmail.com>
2015-03-13 15:27:55 +0000

    Revert "new repo for odin, as whole"
    This reverts commit b729bd6c71e6e14c71dbfa6654f4e4556f05559b.

--- ruby/pine_exercices/name.rb
+++
@@ -1,8 +0,0 @@
-puts 'Hello there, and what\'s your first name?'
-f_name = gets.chomp
-puts 'Hello there, and what\'s your middle name?'
-m_name = gets.chomp
-puts 'Hello there, and what\'s your last name?'
-l_name = gets.chomp
-puts 'Your name is ' + f_name + " "+ m_name+" "+l_name + '?  What a lovely name!'
-puts 'Pleased to meet you, ' + f_name + " "+ m_name+" "+l_name + '.  :)'
