38a4467f9498b180d285d94eb416ca0d361c2c13
lockemind <pedr.amaral@gmail.com>
2015-03-25 12:15:42 +0000

    added edit,delete

--- rails/blogger/app/controllers/articles_controller.rb
+++ rails/blogger/app/controllers/articles_controller.rb
@@ -1,10 +1,35 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
- def index
- 	@articles = Article.all
- end
+  def index
+    @articles = Article.all
+  end
 
- def show
- 	@article = Article.find(params[:id])
- end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+
+  end
+
 end
